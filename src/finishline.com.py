1e3c0ded0b5017c07ee0929fd9cc6f797d401be7
lehieu2017ct <lehieu2017ct@gmail.com>
2019-12-23 23:41:58 +0700

    pull and save data to file

--- src/finishline.com.py
+++ src/finishline.com.py
@@ -1,35 +1,37 @@
 import asyncio
+import json
 from pyppeteer import launch
 
-#   Crawler
-    # We crawl the product by url https://www.nike.com/w?q=AT6174-005
-    # And check the product on the url http://oco.vn/san-pham?q=AT6174-005
-
-    # This is the list of SKU for testing:
-    # 13	AT6174-005	NIKE AIR MAX 270 REACT	Women	4,449,000	Stocking	
-    # 14	AT6174-100	NIKE AIR MAX 270 REACT	Women	4,199,000	Stocking	
-    # 15	AT6174-102	NIKE AIR MAX 270 REACT	Women	4,199,000	Stocking	
-    # 16	AT6174-700	NIKE AIR MAX 270 REACT	Women	4,199,000	Stocking
-
-_nike_url = 'https://www.nike.com/w?q=AT6174-005'
-_nike_product_class = '.product-card__body'
-_nike_product_price = '.original-price'
-
+_finishline_url = 'https://www.finishline.com/store/product/womens-adidas-nmd-r1-casual-shoes/prod3030000?styleId=FW5278&colorId=001'
+_finishline_product_class = '#productSizes .block-grid--sizes div button'
 
 async def main():
     browser = await launch({'headless': False})
     page = await browser.newPage()
 
-    await page.goto(_nike_url)
-    
-    # document.querySelectorAll('.product-card__body')[0]
-    product_el = await page.querySelector(_nike_product_class)
-    await page.click(_nike_product_class)
-
+    await page.goto(_finishline_url, { 'timeout': 80000, 'waitUntil': 'domcontentloaded' })
     # document.querySelectorAll('.original-price')[0].innerText
-    product_el_price = await page.querySelector(_nike_product_price)
-    product_price = await page.evaluate('(element) => element.textContent', product_el_price)
-    
+    product_sizes = await page.querySelectorAll(_finishline_product_class)
+    f = open("output-finishline.com.json", "w")
+    f.write("[")
+    for size in product_sizes:
+        itemSize = await page.evaluate('(element) => element.dataset.size', size)
+        sizeClass =(str)( await page.evaluate('(element) => element.className', size))
+        data = {}
+        if sizeClass.find('disabled') < 0 :
+            data['size'] = itemSize
+            data['quantity'] = 3
+            jsonData = json.dumps(data)
+            f.writelines(jsonData + ', ')
+        else :
+            data['size'] = itemSize
+            data['quantity'] = 0
+            jsonData = json.dumps(data)
+            f.writelines(jsonData + ', ')
+
+    f.write("]")
+    f.close()
+
     await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
