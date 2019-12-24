4116c8f00e30be05f91067253f6f798deb7ca00a
lehieu2017ct <lehieu2017ct@gmail.com>
2019-12-25 02:02:02 +0700

    update for adidas

--- src/adidas.com.py
+++ src/adidas.com.py
@@ -1,35 +1,34 @@
 import asyncio
+import json
 from pyppeteer import launch
 
-#   Crawler
-    # We crawl the product by url https://www.nike.com/w?q=AT6174-005
-    # And check the product on the url http://oco.vn/san-pham?q=AT6174-005
-
-    # This is the list of SKU for testing:
-    # 13	AT6174-005	NIKE AIR MAX 270 REACT	Women	4,449,000	Stocking	
-    # 14	AT6174-100	NIKE AIR MAX 270 REACT	Women	4,199,000	Stocking	
-    # 15	AT6174-102	NIKE AIR MAX 270 REACT	Women	4,199,000	Stocking	
-    # 16	AT6174-700	NIKE AIR MAX 270 REACT	Women	4,199,000	Stocking
-
-_nike_url = 'https://www.nike.com/w?q=AT6174-005'
-_nike_product_class = '.product-card__body'
-_nike_product_price = '.original-price'
-
+_adidas_url = 'https://www.adidas.com/us/ultraboost-19-shoes/G27511.html'
+_adidas_product_class = '.square-list ul li button'
+_adidas_dropdown_class = 'gl-custom-dropdown__select'
 
 async def main():
     browser = await launch({'headless': False})
     page = await browser.newPage()
 
-    await page.goto(_nike_url)
-    
-    # document.querySelectorAll('.product-card__body')[0]
-    product_el = await page.querySelector(_nike_product_class)
-    await page.click(_nike_product_class)
+    # set timeout vi mang yeu
+    await page.goto(_adidas_url, {'timeout': 80000, 'waitUntil': 'domcontentloaded'})
+    if await page.J('.gl-modal__main-content'):
+        await page.click('div > div > a')
+    await page.click('.gl-custom-dropdown__select')
 
-    # document.querySelectorAll('.original-price')[0].innerText
-    product_el_price = await page.querySelector(_nike_product_price)
-    product_price = await page.evaluate('(element) => element.textContent', product_el_price)
-    
+    product_sizes = await page.querySelectorAll(_adidas_product_class)
+    f = open("output-adidas.com.json", "w")
+    f.write("[")
+    for size in product_sizes:
+        itemSize = await page.evaluate('(element) => element.title', size)
+        data = {}
+        data['size'] = itemSize
+        data['quantity'] = 3
+        jsonData = json.dumps(data)
+        f.writelines(jsonData + ', ')
+    f.write("]")
+    f.close()
+
     await browser.close()
 
 asyncio.get_event_loop().run_until_complete(main())
