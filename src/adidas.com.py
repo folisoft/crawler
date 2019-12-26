54b3565d0751b7df0b57c52c31f3d04c14f7dcac
lehieu2017ct <lehieu2017ct@gmail.com>
2019-12-27 00:26:13 +0700

    test adidas

--- src/adidas.com.py
+++ src/adidas.com.py
@@ -11,21 +11,24 @@ async def main():
     page = await browser.newPage()
 
     # set timeout vi mang yeu
-    await page.goto(_adidas_url, {'timeout': 80000, 'waitUntil': 'domcontentloaded'})
-    if await page.J('#modal-root .gl-modal'):
-        await page.click('button')
+    await page.goto(_adidas_url, {'timeout': 80000, 'waitUntil': 'load'})
 
-    product_sizes = await page.querySelectorAll('.gl-custom-dropdown--no-max-height select option')
+    # if await page.J('#modal-root .gl-modal'):
+    #     await page.click('button')
+
+    # product_sizes = await page.querySelectorAll('.gl-custom-dropdown--no-max-height select option')
+    await page.waitForSelector('.square-list ul li', {'timeout': 300000})
+    product_sizes = await page.querySelectorAll('.square-list ul li')
     f = open("output-adidas.com.json", "w")
-    f.write("[")
-    for size in product_sizes:
-        itemSize = await page.evaluate('(element) => element.title', size)
-        data = {}
-        data['size'] = itemSize
-        data['quantity'] = 3
-        jsonData = json.dumps(data)
-        f.writelines(jsonData + ', ')
-    f.write("]")
+    # f.write("[")
+    # for size in product_sizes:
+    #     itemSize = await page.evaluate('(element) => element.title', size)
+    #     data = {}
+    #     data['size'] = itemSize
+    #     data['quantity'] = 3
+    #     jsonData = json.dumps(data)
+    #     f.writelines(jsonData + ', ')
+    # f.write("]")
     f.close()
 
     await browser.close()
