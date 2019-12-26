bdce894592ddef5a0ba50aa18617725306486c32
lehieu2017ct <lehieu2017ct@gmail.com>
2019-12-26 23:30:40 +0700

    update finishline py

--- src/finishline.com.py
+++ src/finishline.com.py
@@ -3,7 +3,11 @@ import json
 from pyppeteer import launch
 
 _finishline_url = 'https://www.finishline.com/store/product/womens-adidas-nmd-r1-casual-shoes/prod3030000?styleId=FW5278&colorId=001'
+
 _finishline_product_class = '#productSizes .block-grid--sizes div button'
+_breadcrumbs_class = '.breadcrumbs li a span'
+
+_shoes_name_class = '#title'
 
 async def main():
     browser = await launch({'headless': False})
@@ -11,9 +15,20 @@ async def main():
 
     await page.goto(_finishline_url, { 'timeout': 80000, 'waitUntil': 'domcontentloaded' }) #set timeout vi mang yeu
 
-    product_sizes = await page.querySelectorAll(_finishline_product_class)
+    data_crawler = {}
+    sizes = []
+    # NAME OF SHOES
+    shoesnames = await page.querySelector(_shoes_name_class)
+    shoesname = await page.evaluate('(element) => element.textContent', shoesnames)
+    data_crawler["name"] = shoesname.strip()
+
+    # GENDER
+    breadcrumbs = await page.querySelectorAll(_breadcrumbs_class)
+    gender_object = await page.evaluate('(element) => element.textContent', breadcrumbs[1])
+    data_crawler["gender"] = gender_object.strip()
+
     f = open("output-finishline.com.json", "w")
-    f.write("[")
+    product_sizes = await page.querySelectorAll(_finishline_product_class)
     for size in product_sizes:
         itemSize = await page.evaluate('(element) => element.dataset.size', size)
         sizeClass =(str)( await page.evaluate('(element) => element.className', size))
@@ -21,15 +36,12 @@ async def main():
         if sizeClass.find('disabled') < 0 :
             data['size'] = itemSize
             data['quantity'] = 3
-            jsonData = json.dumps(data)
-            f.writelines(jsonData + ', ')
         else :
             data['size'] = itemSize
             data['quantity'] = 0
-            jsonData = json.dumps(data)
-            f.writelines(jsonData + ', ')
-
-    f.write("]")
+        sizes.append(data)
+    data_crawler["sizes"] = sizes
+    f.write(json.dumps(data_crawler))
     f.close()
 
     await browser.close()
