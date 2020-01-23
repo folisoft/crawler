19ee7c00d76f37d43c3424aba8f75342a343bf14
lehieu2017ct <lehieu2017ct@gmail.com>
2020-01-23 12:04:41 +0700

    update for nike

--- src/nike.com.py
+++ src/nike.com.py
@@ -1,66 +1,42 @@
-#   Crawler
-    # We crawl the product by url https://www.nike.com/w?q=AT6174-005
-    # And check the product on the url http://oco.vn/san-pham?q=AT6174-005
-
-    # This is the list of SKU for testing:
-    # 13	AT6174-005	NIKE AIR MAX 270 REACT	Women	4,449,000	Stocking	
-    # 14	AT6174-100	NIKE AIR MAX 270 REACT	Women	4,199,000	Stocking	
-    # 15	AT6174-102	NIKE AIR MAX 270 REACT	Women	4,199,000	Stocking	
-    # 16	AT6174-700	NIKE AIR MAX 270 REACT	Women	4,199,000	Stocking
-
 import asyncio
 import json
 from pyppeteer import launch
-import pydash
-import json
 
-_output = {
-    "name": "Women's adidas NMD R1 Casual Shoes",
-    "gender": "Women",
-    "sizes": []
-}
-
-_size_item = {
-    "size": "5.5",
-    "quantity": 0
-}
-
-_page_name = 'nike.com'
-_detail_url = 'https://www.nike.com/t/air-max-270-react-womens-shoe-6KXPPH/AT6174-200'
-
+_nike_url = 'https://www.nike.com/t/air-max-270-react-womens-shoe-6KXPPH/AT6174-200'
+_nike_gender_class = 'h2[data-test="product-sub-title"]'
+_nike_sizes_class = '#buyTools fieldset div > div > input'
 async def main():
-    # pyppeteer
     browser = await launch({'headless': False})
-    page = await browser.newPage()
+    context = await browser.createIncognitoBrowserContext()
+    page = await context.newPage()
+    await page.setViewport({'width': 1600, 'height': 1300})
     await page.setUserAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64)\
                                 AppleWebKit/537.36 (KHTML, like Gecko) \
                                 Chrome/66.0.3359.181 Safari/537.36")
-    await page.goto(_detail_url, {'timeout': 0 })
+    await page.goto(_nike_url, {'timeout': 0})
 
-    # product name
-    class_name = '#pdp_product_title'
-    element = await page.querySelector(class_name)
-    content = await page.evaluate('(element) => element.textContent', element)
-    _output["name"] = content.strip()
+    data_crawler = {}
+    final_sizes = []
 
-    # gender
-    class_name = '.headline-5.pb1-sm'
-    element = await page.querySelector(class_name)
-    content = await page.evaluate('(element) => element.textContent', element)
-    _output["gender"] = content.strip()
+    # GENDER
+    gender_query = "Array.from(document.querySelectorAll('"+_nike_gender_class+"')).map(item => item.textContent);"
+    genders = await page.evaluate(gender_query, force_expr=True)
 
-    # size listing
-    class_name = '.css-1uentg input'
-    await page.waitForSelector(".css-1uentg input", { "visible": True })
-    document_query = "Array.from(document.querySelectorAll('.css-1uentg input')).map(x=> {return {quantity: (x.disabled ? 3 : 0), size: (x.value.split(':')[1])}});"
-    sizes = await page.evaluate(document_query, force_expr=True)
+    data_crawler['Gender'] = 'Men' if 'Men' in genders[0] else 'Women'
 
-    print(sizes)
-    _output["sizes"] = sizes
+    # SIZES
+    query = "Array.from(document.querySelectorAll('"+_nike_sizes_class+"')).map(item => ({disabled: item.disabled, size: item.value.split(':')[1]}));"
+    product_sizes = await page.evaluate(query, force_expr=True)
+    for item in product_sizes:
+        dataItem = {}
+        dataItem['size'] = item['size']
+        dataItem['quantity'] = 0 if item['disabled'] == 'true' else 3
+        final_sizes.append(dataItem)
+    data_crawler["sizes"] = final_sizes
 
-    # file json
-    f = open(_page_name+'.json', "w")
-    f.write(json.dumps(_output))
+    # WRITE TO FILE
+    f = open("output-nike.com.json", "w")
+    f.write(json.dumps(data_crawler))
     f.close()
 
     await browser.close()
